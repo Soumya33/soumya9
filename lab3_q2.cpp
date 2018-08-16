8fde2bb6948305df05218f59ca3d4127115b75c9
Soumya33 <42139622+Soumya33@users.noreply.github.com>
2018-08-16 10:58:02 -0400

    Add files via upload

--- lab3_q2.cpp
+++ lab3_q2.cpp
@@ -8,11 +8,18 @@ int a,b,sum,substract,product,remainder ; // declaration
 float division;
 a = 5; // definition
 b = 3;  // definition
+float x, y,sum2,substract2,product2,division2;
+x = 2.678;
+y = 7.987;
 sum = a + b; // adds 2 numbers
 substract = a - b; // calculates the difference
 product = a * b; // calculates the product
 division = a / b; // divides the numbers
 remainder = a % b; // finds the remainder
+sum2 = x + y; // adds 2 numbers
+substract2 = y - x; // calculates the difference
+product2 = x * y; // calculates the product
+division2 = y / a; // divides the numbers
 // displaying the calculations in terminal
 cout << "numbers are " << a << "," << b << endl; 
 cout << "results in integer" << endl;
@@ -21,6 +28,11 @@ cout << "difference is " << substract << endl;
 cout << "product is " << product << endl;
 cout << "quotient is " << division << endl;
 cout << "remainder is " << remainder << endl;
+cout << "results in float" << endl;
+cout << "sum is " << sum2 << endl;
+cout << "difference is " << substract2 << endl;
+cout << "product is " << product2 << endl;
+cout << "quotient is " << division2 << endl;
 return 0;
 }
 
